791352c76d845762b908ffebf65bc0191eeb683e
Keerthana Srinivasan <140654812+Keerthana07S@users.noreply.github.com>
2025-06-19 10:22:29 -0400

    Update QUINN_backend.cc

--- QUINN_backend.cc
+++ QUINN_backend.cc
@@ -1,44 +1,54 @@
 #include <Adafruit_NeoPixel.h>
+#include <Complex.h>
 
-#define LED_PIN    6       
-#define NUM_LEDS   9     
+#define LED_PIN    12       
+#define NUM_LEDS   16     
 
-#define H_GATE_PIN 2
-#define X_GATE_PIN 3
-#define S_GATE_PIN 4
-#define I_GATE_PIN 5
+#define H_GATE_PIN 7
+#define X_GATE_PIN 4
+#define S_GATE_PIN 2
+#define I_GATE_PIN 8
+
+const Complex i(0, 1);
 
 Adafruit_NeoPixel strip(NUM_LEDS, LED_PIN, NEO_GRB + NEO_KHZ800);
 
+//note: initially, these matrices would be 2X2 matrices, but I used the Kronecker product to make them 4X4
+//now, the activity is more fun! :)
+
 //identity matrix (usually when there isn't a gate in the quantum circuit)
-int identity[3][3] = {
-  {1, 0, 0},
-  {0, 1, 0},
-  {0, 0, 1}
+int identity[4][4] = {
+  {1, 0, 0, 0},
+  {0, 1, 0, 0},
+  {0, 0, 1, 0},
+  {0, 0, 0, 1},
 };
 
 //hadamard matrix
-int hadamard[3][3] = {
-  {1, 1, 0},
-  {1, -1, 0},
-  {0, 0, 1}
+int hadamard[4][4] = {
+  {1, 1, 1, 1},
+  {1, -1, 1, -1},
+  {1, 1, -1, -1},
+  {1, -1, -1, 1},
 };
 
-//x gate
-int xgate[3][3] = {
-  {0, 1, 0},
-  {1, 0, 0},
-  {0, 0, 1}
+//pauli-x gate
+int xgate[4][4] = {
+  {0, 0, 0, 1},
+  {0, 0, 1, 0},
+  {0, 1, 0, 0},
+  {1, 0, 0, 0},
 };
 
 //s gate
-int sgate[3][3] = {
-  {1, 0, 0},
-  {0, 0, -1},
-  {0, 1, 0}
+int sgate[4][4] = {
+  {1, 0, 0, 0},
+  {0, i, 0, 0},
+  {0, 0, i, 0},
+  {0, 0, 0, i**2},
 };
 
-int result[3][3];
+int result[4][4];
 
 void setup() {
   strip.begin();
@@ -86,24 +96,24 @@ void loop() {
   delay(250)
 }
 
-void copyMatrix(int source[3][3], int dest[3][3]) {
-  for (int i = 0; i < 3; i++)
-    for (int j = 0; j < 3; j++)
+void copyMatrix(int source[4][4], int dest[4][4]) {
+  for (int i = 0; i < 4; i++)
+    for (int j = 0; j < 4; j++)
       dest[i][j] = source[i][j];
 }
 
-void multiplyMatrix(int gate[3][3], int mat[3][3]) {
-  int temp[3][3] = {0};
-  for (int i = 0; i < 3; i++) {
-    for (int j = 0; j < 3; j++) {
-      for (int k = 0; k < 3; k++) {
+void multiplyMatrix(int gate[4][4], int mat[4][4]) {
+  int temp[4][4] = {0};
+  for (int i = 0; i < 4; i++) {
+    for (int j = 0; j < 4; j++) {
+      for (int k = 0; k < 4; k++) {
         temp[i][j] += gate[i][k] * mat[k][j];
       }
     }
   }
 
   //
-  for (int i = 0; i < 3; i++)
-    for (int j = 0; j < 3; j++)
+  for (int i = 0; i < 4; i++)
+    for (int j = 0; j < 4; j++)
       mat[i][j] = temp[i][j];
 }
